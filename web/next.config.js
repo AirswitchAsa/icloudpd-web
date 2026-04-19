86fc9091ddb37e16d55dd062d7ed84a8cdf13b61
spicadust <spicastre@gmail.com>
2026-04-19 17:27:50 +0200

    chore(web): remove old Next.js frontend

--- web/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Enable static HTML export
-  trailingSlash: true,  // Recommended for static hosting
-}
-
-module.exports = nextConfig
